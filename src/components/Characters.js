2e8f253ec6e8f94c2be0e83a6cd96b48948b42de
Paula Galdames <sitio.galdames@gmail.com>
2023-01-25 00:56:05 -0300

    Aplicacion con la api

---
+++ src/components/Characters.js
@@ -0,0 +1,50 @@
+export default function Characters(props) {
+    const {characters, setCharacters} = props;
+
+    const resetCharacters = () => {
+        setCharacters(null)
+    }
+
+  return (
+    <div className="characters">
+        <h1>
+            Personajes
+        </h1>
+        <span className="back-home" onClick={resetCharacters}>Volver al inicio</span>
+        <div className="container-characters">
+            {characters.map((characters, index) => (
+                <div className="character-container" key={index}>
+                    <div>
+                        <img src={characters.image} alt={characters.name} />
+                    </div>
+                    <div>
+                        <h4>{characters.name}</h4>
+                        <h6>
+                            {characters.status === "Alive" ?(
+                                <>
+                                    <span className="alive" />
+                                    Alive
+                                </>
+                            ) : (
+                                <>
+                                    <span className="dead" />
+                                    Dead
+                                </>
+                            )}
+                        </h6>
+                        <p>
+                            <span className="text-grey">Episodios </span>
+                            <span>{characters.episode.length}</span>
+                        </p>
+                        <p>
+                            <span className="text-grey">Especie: </span>
+                            <span>{characters.species}</span>
+                        </p>
+                    </div>
+                </div>
+            ))}
+        </div>
+        <span className="back-home" onClick={resetCharacters}>Volver al inicio</span>
+    </div>
+  )
+}
